c48d738836933ab1968ffbee46e223973097c412
davidd9 <davidd9@gmail.com>
2026-02-16 19:11:51 +0000

    File changes

--- src/components/announcements/AnnouncementTicker.jsx
+++ src/components/announcements/AnnouncementTicker.jsx
@@ -29,7 +29,7 @@ export default function AnnouncementTicker() {
         <div className="flex-1 relative overflow-hidden h-6">
           <motion.div
             className="absolute top-0 left-0 text-sm font-medium text-blue-900 dark:text-blue-100 whitespace-nowrap"
-            animate={{ x: ['0%', '-50%'] }}
+            animate={{ x: ['-50%', '0%'] }}
             transition={{
               duration: 60,
               repeat: Infinity,
